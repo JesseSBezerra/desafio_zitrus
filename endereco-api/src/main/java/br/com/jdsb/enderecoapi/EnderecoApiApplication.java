d66b2192de9a2cd559a7ec2675e9a20ffa226c78
JesseSBezerra <jessebezerra@hotmail.com.br>
2022-09-25 12:38:59 -0300

    adição de apis ao balanceador e adição do gateway

--- endereco-api/src/main/java/br/com/jdsb/enderecoapi/EnderecoApiApplication.java
+++ endereco-api/src/main/java/br/com/jdsb/enderecoapi/EnderecoApiApplication.java
@@ -2,8 +2,10 @@ package br.com.jdsb.enderecoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EnderecoApiApplication {
 
     public static void main(String[] args) {
--- gateway-api/src/main/java/br/com/jdsb/gatewayapi/GatewayApiApplication.java
+++ gateway-api/src/main/java/br/com/jdsb/gatewayapi/GatewayApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.jdsb.clienteapi;
+package br.com.jdsb.gatewayapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ClienteApiApplication {
+public class GatewayApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClienteApiApplication.class, args);
+        SpringApplication.run(GatewayApiApplication.class, args);
     }
 
 }
